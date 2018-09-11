aa34d37b44a68312d4643dd0ffa53e2460328ea2
Alan <alanzam@msn.com>
2018-09-11 18:01:28 -0500

    First Commit

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 
+global.eventHub = new EventEmitter();
+
 render(
   <App />,
   document.getElementById('myApp')
